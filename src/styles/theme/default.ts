9437c79c1dd3110e2c877597f552394e56a72683
Carlos Oliveira <carlos.burndev@gmail.com>
2023-11-01 00:34:22 -0300

    style: adds default font sizes and fix color yellow-dark

--- src/styles/theme/default.ts
+++ src/styles/theme/default.ts
@@ -1,21 +1,40 @@
 export const defaultTheme = {
-  'yellow-dark': '#C4F17',
-  yellow: '#DBAC2C',
-  'yellow-light': '#F1E9C9',
+  colors: {
+    'yellow-dark': '#C47F17',
+    yellow: '#DBAC2C',
+    'yellow-light': '#F1E9C9',
 
-  'purple-dark': '#4B2995',
-  purple: '#8047F8',
-  'purple-light': '#EBE5F9',
+    'purple-dark': '#4B2995',
+    purple: '#8047F8',
+    'purple-light': '#EBE5F9',
 
-  'base-title': '#272221',
-  'base-subtitle': '#403937',
-  'base-text': '#574F4D',
-  'base-label': '#8D8686',
-  'base-hover': '#D7D5D5',
-  'base-button': '#E6E5E5',
-  'base-input': '#EDEDED',
-  'base-card': '#F3F2F2',
+    'base-title': '#272221',
+    'base-subtitle': '#403937',
+    'base-text': '#574F4D',
+    'base-label': '#8D8686',
+    'base-hover': '#D7D5D5',
+    'base-button': '#E6E5E5',
+    'base-input': '#EDEDED',
+    'base-card': '#F3F2F2',
 
-  background: '#FAFAFA',
-  white: '#FFFFFF',
+    background: '#FAFAFA',
+    white: '#FFFFFF',
+  },
+
+  fontSizes: {
+    'title-xl': '3rem', // 48px
+    'title-l': '2rem', // 32px
+    'title-m': '1.5rem', // 24px
+    'title-s': '1.25rem', // 20px
+    'title-xs': '1.125rem', // 18px
+
+    'text-l': '1.25rem', // 20px
+    'text-m': '1rem', // 16px
+    'text-s': '0.875rem', // 14px
+    'text-xs': '0.75rem', // 12px
+
+    tag: '0.625', // 10px
+    'button-g': '0.875', // 14px
+    'button-m': '0.75', // 12px
+  },
 }
